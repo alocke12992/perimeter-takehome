d182c030d6ad351a9d6be52b172291a9bae44e5e
Andrew Locke <andrew.locke@spiral.us>
2024-07-19 09:29:22 -1000

    list polygons by sessions

--- server/src/routes/SessionRoutes.ts
+++ server/src/routes/SessionRoutes.ts
@@ -1,13 +1,24 @@
+import { ObjectId } from "mongodb";
 import HttpStatusCodes from "@src/common/HttpStatusCodes";
 import { IReq, IRes } from "./types/express/misc";
 import { ISession } from "@src/models/Session";
 import SessionsController from "@src/controllers/SessionsController";
+import PolygonsController from "@src/controllers/PolygonsController";
 
-export const createSession = async (
-  req: IReq<Omit<ISession, "createdAt">>,
-  res: IRes
-) => {
+const create = async (req: IReq<Omit<ISession, "createdAt">>, res: IRes) => {
   const { lat, long } = req.body;
   const session = await SessionsController.create({ lat, long });
   return res.status(HttpStatusCodes.CREATED).json({ session });
+};
+
+const listSessionPolygons = async (req: IReq, res: IRes) => {
+  const { id } = req.params;
+  console.log("SESSION ID", id);
+  const polygons = await PolygonsController.listBySessionId(new ObjectId(id));
+  return res.status(HttpStatusCodes.OK).json({ polygons });
+};
+
+export default {
+  create,
+  listSessionPolygons,
 };
