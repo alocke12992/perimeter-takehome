dde463c36712b3c94324feff443ecc7c14fc43ae
Andrew Locke <andrew.locke@spiral.us>
2024-07-19 06:43:34 -1000

    removes generated files

--- server/src/common/Paths.ts
+++ server/src/common/Paths.ts
@@ -2,14 +2,6 @@
  * Express router paths go here.
  */
 
-
 export default {
-  Base: '/api',
-  Users: {
-    Base: '/users',
-    Get: '/all',
-    Add: '/add',
-    Update: '/update',
-    Delete: '/delete/:id',
-  },
+  Base: "/api",
 } as const;
